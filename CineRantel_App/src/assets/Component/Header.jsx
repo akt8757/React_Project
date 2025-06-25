9ba2e10e24e8b53936dfce6e1c34f4eade045783
Asraful Kabir <asrafulkabir017@gmail.com>
2025-06-25 15:46:54 +0600

    add shoping cart

--- CineRantel_App/src/assets/Component/Header.jsx
+++ CineRantel_App/src/assets/Component/Header.jsx
@@ -1,43 +1,50 @@
 import React from "react";
+import { useState } from "react";
 import logo from "../icons/logo.svg";
 import gear from "../icons/gear.svg";
 import moon from "../icons/moon.svg";
 import cart from "../icons/shopping-cart.svg";
+import ShoppingCart from "./ShoppingCart";
 export default function Header() {
+  const [isCart, setIsCart] = useState(false);
   return (
-    <header>
-      <nav className="container flex items-center justify-between space-x-10 py-6">
-        <a href="index.html">
-          <img src={logo} width="139" height="26" alt="img" />
-        </a>
+    <>
+      {isCart && <ShoppingCart onCloseCart={() => setIsCart(false)} />}
+      <header>
+        <nav className="container flex items-center justify-between space-x-10 py-6">
+          <a href="index.html">
+            <img src={logo} width="139" height="26" alt="img" />
+          </a>
 
-        <ul className="flex items-center space-x-5">
-          <li>
-            <a
-              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href="#"
-            >
-              <img src={gear} width="24" height="24" alt="" />
-            </a>
-          </li>
-          <li>
-            <a
-              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href="#"
-            >
-              <img src={moon} width="24" height="24" alt="" />
-            </a>
-          </li>
-          <li>
-            <a
-              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href="#"
-            >
-              <img src={cart} width="24" height="24" alt="" />
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+          <ul className="flex items-center space-x-5">
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <img src={gear} width="24" height="24" alt="" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <img src={moon} width="24" height="24" alt="" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+                onClick={() => setIsCart(true)}
+              >
+                <img src={cart} width="24" height="24" alt="" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
