7de8145f667c8459df3f6d267b83712270c13a0a
Asraful Kabir <asrafulkabir017@gmail.com>
2025-07-18 05:11:23 +0600

    userdownlod and set perams done with provider

---
+++ image-generat-ai/src/Hooks/useDownload.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const useDownload = () => {
+  const [isDownloadImage, setIsDownload] = useState([]);
+  const [downloading, setDownloading] = useState(false);
+
+  const handleDownloadImage = async (imageUrl) => {
+    try {
+      setDownloading(true);
+      const response = await fetch(imageUrl);
+      const getBlob = await response.blob();
+
+      // image  downloading logic
+      const blobUrl = URL.createObjectURL(getBlob);
+      const a = document.createElement("a");
+      a.href = blobUrl;
+      a.download = `image-${Date.now()}.jpg`;
+      a.click();
+      URL.revokeObjectURL(blobUrl);
+
+      // get image url data
+      const found = isDownloadImage.find((item) => item.url === imageUrl);
+      const url = new URL(imageUrl);
+
+      if (!found) {
+        const getParms = {
+          width: url.searchParams.get("width"),
+          height: url.searchParams.get("height"),
+          seed: url.searchParams.get("seed"),
+          model: url.searchParams.get("model"),
+          url: imageUrl,
+        };
+        setIsDownload((prev) => [...prev, getParms]);
+      }
+    } catch (eror) {
+      console.log("error", eror);
+    } finally {
+      setDownloading(false);
+    }
+  };
+  return {
+    downloading,
+    handleDownloadImage,
+    isDownloadImage,
+  };
+};
+export default useDownload;
--- image-generat-ai/src/Store.jsx
+++ image-generat-ai/src/Store.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import useFetchApi from "./Hooks/useFetchApi";
 import { apiImageProvider } from "./assets/Provider";
+import { downloadImageContext } from "./assets/Provider";
+import useDownload from "./Hooks/useDownload";
 
 export default function Store({ children }) {
   const apiData = useFetchApi();
+  const downLoadedImage = useDownload();
 
   return (
     <div>
       <apiImageProvider.Provider value={{ apiData }}>
-        {children}
+        <downloadImageContext.Provider value={{ downLoadedImage }}>
+          {children}
+        </downloadImageContext.Provider>
       </apiImageProvider.Provider>
     </div>
   );
