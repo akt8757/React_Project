e996cb8a5ca8fbdf7f63d54da96c446b53b61c80
Asraful Kabir <asrafulkabir017@gmail.com>
2025-07-31 13:55:07 +0600

    add 2 component and specyfy product detail page

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+import ProductDetail from "./assets/Component/productDetail";
+import ProductList from "./assets/Component/ProductList";
+
 function App() {
+  const [isId, setIsId] = useState(1);
+  const igetIdHandeler = (id) => {
+    setIsId(id);
+  };
   return (
-    <>
-      <h1>this is kabir</h1>
-    </>
+    <div className="flex h-screen">
+      <div className="w-3/5 overflow-y-auto ... h-full">
+        <ProductList igetIdHandeler={igetIdHandeler} />
+      </div>
+      <div className="w-2/5">
+        <ProductDetail id={isId} />
+      </div>
+    </div>
   );
 }
 
---
+++ react-query/src/assets/Component/ProductList.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+export default function ProductList({ igetIdHandeler }) {
+  const getProductData = async ({ queryKey }) => {
+    const products = await axios.get(`http://localhost:3001/${queryKey[0]}`);
+    return products.data;
+  };
+  getProductData();
+  const { error, data, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: getProductData,
+  });
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) return <h1>error ocard</h1>;
+  return (
+    <div>
+      <div>
+        <ul className="flex flex-wrap justify-center items-center ">
+          {data &&
+            data.map((item) => (
+              <li
+                onClick={() => igetIdHandeler(item.id)}
+                className=" flex flex-col border border-sky-200 rounded-sm m-2"
+              >
+                <h2 className="text-xl my-2 text-center">{item.title}</h2>
+                <img
+                  className="h-60 w-80 object-cover "
+                  src={item.thumbnail}
+                  alt={item.title}
+                />
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
