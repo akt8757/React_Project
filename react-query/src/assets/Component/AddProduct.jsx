4f8bf26e8c460329fc2decf3666440e055148807
akt8757 <asrafulkabir017@gmail.com>
2025-08-06 03:14:42 +0600

    addd deleite  option

--- react-query/src/assets/Component/AddProduct.jsx
+++ react-query/src/assets/Component/AddProduct.jsx
@@ -1,7 +1,7 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import React, { useState } from "react";
-import { message, Space } from "antd";
+import { Form, message, Space } from "antd";
 
 export default function AddProduct() {
   const [messageApi, contextHolder] = message.useMessage();
@@ -18,15 +18,21 @@ export default function AddProduct() {
       content: "This is a success message",
     });
   };
-  const { mutate, isSuccess, status, submittedAt } = useMutation({
-    mutationFn: (product) => {
-      const singleProduct = axios.post(
-        "http://localhost:3001/products",
-        product
-      );
-      return singleProduct;
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: (product) =>
+      axios.post("http://localhost:3001/products", product),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["products"]);
     },
   });
+
+  const handleSubmit = () => {
+    // e.preventDefault();
+    console.log("submitting");
+    const newData = { ...state, id: crypto.randomUUID().toString() };
+    mutation.mutate(newData);
+  };
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
@@ -36,17 +42,15 @@ export default function AddProduct() {
       [name]: value,
     });
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newData = { ...state, id: crypto.randomUUID().toString() };
-    mutate(newData);
-  };
-  submittedAt && success();
+
   return (
     <div className="bg-gray-100 m-3 w-full p-2">
       {contextHolder}
       <h2 className="text-2xl text-center">Add product</h2>
-      <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+      <form
+        // onSubmit={handleSubmit}
+        className="flex flex-col items-center"
+      >
         <input
           className="border-cyan-500 border-1 p-2 rounded my-2 w-full"
           placeholder="Product Title"
@@ -91,13 +95,13 @@ export default function AddProduct() {
           value={state.thumbnail}
           onChange={handleChange}
         />
-        <button
-          type="submit"
-          className="rounded-2xl bg-amber-700 border py-2 px-8 text-amber-50 btn"
-        >
-          Submit
-        </button>
       </form>
+      <button
+        onClick={handleSubmit}
+        className="rounded-2xl bg-amber-700 border py-2 px-8 text-amber-50"
+      >
+        Submit
+      </button>
     </div>
   );
 }
